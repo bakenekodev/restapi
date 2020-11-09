fd133736262a5d042f48c1438f4d04abf3485e49
Bitca Marius <bitca.marius@isa.utm.md>
2020-11-09 22:53:04 +0200

    added queries parsing

--- main.go
+++ main.go
@@ -11,11 +11,15 @@ import (
 	"github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/proxy"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	goyesql "github.com/nleof/goyesql"
 	"golang.org/x/oauth2/google"
 )
 
-// DB - global MySQL database
+// DB is the global MySQL database
 var DB *sql.DB
+
+// Queries is a map that stores all the SQL queries
+var Queries goyesql.Queries
 var err error
 
 func main() {
@@ -24,20 +28,27 @@ func main() {
 	setUpDB()
 	defer DB.Close()
 
+	// Prepare the queries
+	Queries = goyesql.MustParseFile("queries.sql")
+
 	// Init Router
 	r := mux.NewRouter()
 
 	// Route Handlers / Endpoints
+	// User
 	r.HandleFunc("/api/users", GetUsers).Methods("GET")
 	r.HandleFunc("/api/users", CreateUser).Methods("POST")
 	r.HandleFunc("/api/users/{id}", GetUser).Methods("GET")
 	r.HandleFunc("/api/users/{id}", UpdateUser).Methods("PUT")
 	r.HandleFunc("/api/users/{id}", DeleteUser).Methods("DELETE")
 
+	// Driver
+	r.HandleFunc("/api/drivers", CreateDriverTrip).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
-// setUpDB - connects to the Cloud SQL throgh a proxy server
+// setUpDB connects to the Cloud SQL throgh a proxy server
 func setUpDB() {
 	credsFile := "credentials.json"
 	SQLScope := "https://www.googleapis.com/auth/sqlservice.admin"
